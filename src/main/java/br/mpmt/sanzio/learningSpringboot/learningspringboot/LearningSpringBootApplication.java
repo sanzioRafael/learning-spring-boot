630eccf0c5b53d86d9307ccf568d6656dfa48c20
sanzioRafael <rafael.sanzio18@gmail.com>
2018-07-23 10:35:45 -0400

    First Commit

---
+++ src/main/java/br/mpmt/sanzio/learningSpringboot/learningspringboot/LearningSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.mpmt.sanzio.learningSpringboot.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+}
